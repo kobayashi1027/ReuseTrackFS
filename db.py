98b334132192db3fd513675825884d942a02113b
kobayashi1027 <koba1027yashi@gmail.com>
2016-10-24 15:09:10 +0900

    Create middle tables(sources, destinations), and associate files with copy_logs through them

--- db.py
+++ db.py
@@ -5,14 +5,15 @@ path = os.path.join(os.path.dirname(__file__), './sqlalchemy/lib')
 sys.path.append(path)
 from sqlalchemy.engine import create_engine
 from sqlalchemy.types import Integer, String, DateTime
-from sqlalchemy.schema import Column
+from sqlalchemy.schema import Column, ForeignKey, ForeignKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relation
 
 Base = declarative_base()
 
 class File(Base):
     __tablename__ = "files"
+
     id = Column(Integer, primary_key=True)
     inode = Column(Integer)
     name = Column(String)
@@ -25,13 +26,33 @@ class File(Base):
     size = Column(Integer)
     hash_value = Column(String)
 
-class CopyLog(Base):
-    __tablename__ = 'copy_logs'
-    id = Column(Integer, primary_key=True)
-    source = Column(Integer)
-    destination = Column(Integer)
+class Source(Base):
+    __tablename__ = "sources"
 
-engine = create_engine('sqlite:///filelog.sqlite3', echo=True)
+    id = Column(Integer, primary_key=True)
+    copy_log_id = Column(Integer, ForeignKey("copy_logs.id"))
+    file_id = Column(Integer, ForeignKey("files.id"))
+    file = relation("File", uselist=False)
+
+class Destination(Base):
+    __tablename__ = "destinations"
+
+    id = Column(Integer, primary_key=True)
+    copy_log_id = Column(Integer, ForeignKey("copy_logs.id"))
+    file_id = Column(Integer, ForeignKey("files.id"))
+    file = relation("File", uselist=False)
+
+class CopyLog(Base):
+    __tablename__ = "copy_logs"
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime)
+
+    # Define relation to File, using middle table ("Source" and "Destination")
+    # example: CopyLog().source = File()
+    source = relation("File", uselist=False, secondary="sources")
+    destination = relation("File", uselist=False, secondary="destinations")
+
+engine = create_engine('sqlite:///filelog.sqlite3', echo=False)
 Base.metadata.create_all(engine)
 
 session = sessionmaker(bind = engine)
